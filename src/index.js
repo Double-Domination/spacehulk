88231d1ff8299579ba64037af71d9c0fda8d1882
D0m1nator <squremeta@gmail.com>
2020-07-18 21:25:24 +0700

    Official react documentation 6 error eliminated. Passing 1 instead of i in if else conditions

--- src/index.js
+++ src/index.js
@@ -5,53 +5,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-
 function Square(props) {
-	return(
+	return (
 		<button className="square" onClick={props.onClick}>
 			{props.value}
 		</button>
-	)
+	);
 }
 
-
 class Board extends React.Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
-		this.state={
+		this.state = {
 			squares: Array(9).fill(null),
-			xIsNext:true,
+			xIsNext: true,
 		};
 	}
 
-	handleClick(i){
-		const squares =this.state.squares.slice();//Perform kind of copying of Array
-		if(calculateWinner (squares) || squares[1]) {
-			console.log("Game ended");
-			return;// console.log("Game ended"); // improvised exit
+	handleClick(i) {
+		const squares = this.state.squares.slice();
+		if (calculateWinner(squares) || squares[i]) {
+			console.log("Unaxeptable argument ))) ");
+			return;
 		}
-		squares[i]=this.state.xIsNext ? 'X': 'O';
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
 		this.setState({
-            squares:squares,
-			xIsNext: !this.state.xIsNext,
-
-		});
+			              squares: squares,
+			              xIsNext: !this.state.xIsNext,
+		              });
 	}
 
 	renderSquare(i) {
-		return( <Square value={this.state.squares[i]}
-		onClick={()=>this.handleClick(i)}
-		/>);
+		return (
+			<Square
+				value={this.state.squares[i]}
+				onClick={() => this.handleClick(i)}
+			/>
+		);
 	}
 
 	render() {
 		const winner = calculateWinner(this.state.squares);
 		let status;
-		if(winner) {
-			status= 'The winner is: ' + winner;
-		}else {
-			status= 'Next turn: ' +(this.state.xIsNext ? 'X' : 'O');
+		if (winner) {
+			status = 'Winner: ' + winner;
+		} else {
+			status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 		}
 
 		return (
@@ -93,32 +92,29 @@ class Game extends React.Component {
 	}
 }
 
-
-function calculateWinner(squares) {
-	const lines=[
-		[0,1,2],
-		[3,4,5],
-		[6,7,8],
-		[0,3,6],
-		[1,4,7],
-		[2,5,8],
-		[0,4,8],
-		[2,4,6],
-	];
-	for (let i=0; i<lines.length; i++) {
-		const [a, b, c]=lines[i]; // tricky destructuring foldings in array
-		if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){
-			//check values in position
-			console.log ("Game ended ");
-			return squares[a];
-		}
-
-	}
-	return null;
-}
 // ========================================
 
 ReactDOM.render(
 	<Game />,
 	document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
+}
