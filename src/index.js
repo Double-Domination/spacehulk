90c84877a8be2f0f9b4c84dc78635d3ab021f77c
D0m1nator <squremeta@gmail.com>
2020-08-10 20:21:09 +0700

    Pragmatic React Book Training

--- src/index.js
+++ src/index.js
@@ -7,52 +7,18 @@ import './index.css';
 
 
 
-
-
-/*Prototype of -=srate=-
-+initial products list
-+ search request(user input)
-+ checkbox state
-+ filtered list of products
-
-array. map
-console representation in jsx
-event object fields
-passing methods as props
-composition three training
-bootstrap quick run
-
-============================
-
-++search request
-++ checkbox state
-
-
- */
-
-function IntMain(props) {
+function Main(){
 	return (
-		<FilterableProductTable products={PRODUCTS} />
+		<div>
+			<h1>Whats going on here</h1>
+		</div>
 	);
 }
 
 
 
 
-
-
-
-
-const PRODUCTS = [
-	{category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-	{category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-	{category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-	{category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-	{category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-	{category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
-
 ReactDOM.render(
-	<IntMain/>,
+	<Main/>,
 	document.getElementById('root')
 );
