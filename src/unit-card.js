42ee5a2123d91aea2317a82dc35297c19de9534f
D0m1nator <squremeta@gmail.com>
2020-08-25 15:38:07 +0700

    Pragmatic React Book Training5 Arrow Function components

--- src/unit-card.js
+++ src/unit-card.js
@@ -30,7 +30,7 @@ devSlava.getDevStats();
 
 //Page 25 Road to React
 
-const list = [
+const stories = [
 	{
 		title:'React',
 		url:'https://reactjs.org',
@@ -49,25 +49,7 @@ const list = [
 	}
 ];
 
-const  List=()=>{
-	return (
-		list.map(function (item){
-			return (
-				<div key ={item.objectID}>
-				        <span>
-					        <a href={item.url}>
-						        {item.title}
-					        </a>
-				        </span>
-				        <span>{item.author}</span>
-				        <span>{item.num_comments}</span>
-				        <span>{item.points}</span>
-			        </div>
-			);
-		})
-	);
 
-}
 
 const deathWingTerminatorSergeant= {
 	      W: 2,
@@ -88,25 +70,71 @@ const deathWingTerminatorSergeant= {
 
 
 
-    const UnitCard=(props)=>{
-        return(
-        	<div
-		        className="standard-unit-card"
-	        >
-		        <h1 className='unit-card-header'>{props.unitCardTitle}</h1>
+const UnitCard=(props)=>{
+	const handleReliquariumChange=(event)=>{
+		setSearchTerm(event.target.value);
+	};
 
-		        <ol>
-			        <List/>
-                </ol>
 
-		        <label htmlFor="wargearSearch">
-			        Search wargear in reliquarium
-		        </label>
-		        <input id="wargearSearch" type="text"/>
-	        </div>
-        );
-    };
+	const searchTermState=useState('');
+	const serchTerm = searchTermState[0];
+	const setSerchTerm= serchTermStrate[1];
+
+	const [searchTerm, setSearchTerm]=React.useState('');
+
+
+    return(
+        <div
+	        className="standard-unit-card"
+        >
+	        <h1 className='unit-card-header'>{props.unitCardTitle}</h1>
+
+	        <ol>
+		        <List list={stories}/>
+            </ol>
+
+	        <label htmlFor="wargearSearch">
+		        Search wargear in reliquarium
+	        </label>
+	        <input
+		        id="wargearSearch"
+		        type="text"
+	            onChange={handleReliquariumChange}
+	        />
+
+	        <p>
+		        Searching for <strong>{searchTerm}</strong>
+	        </p>
+
+
+        </div>
+    );
+};
+
+const  List=(props)=>{
+	return (
+		props.list.map((item)=>{
+			return (
+				<div key ={item.objectID}>
+				        <span>
+					        <a href={item.url}>
+						        {item.title}
+					        </a>
+				        </span>
+				        <span>{item.author}</span>
+				        <span>{item.num_comments}</span>
+				        <span>{item.points}</span>
+			        </div>
+			);
+		})
+	);
+
+};
 
 
 export default UnitCard;
 /*Page 27 Advanced component definition*/
+/*Handler Function in JSX*/
+/*ReactProperties*/
+/*Page 36*/
+/*useState.hook*/
