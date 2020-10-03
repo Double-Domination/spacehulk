ef0216b528fa69026c8c4c8ca62d742dc51aa971
D0m1nator <squremeta@gmail.com>
2020-10-03 23:58:26 +0700

    Massive learning adventure trought VSVIM

--- src/components/UnitCard/UnitCard.js
+++ src/components/UnitCard/UnitCard.js
@@ -7,18 +7,29 @@
  * Unit Card
  */
 import React from "react";
+
 import ReactDOM from "react-dom";
+
 import styled from "styled-jss";
 
+const ajasckHummer = [4, 5, 6, 7, "444"];
+
 document.title = "CSS Manual TurboCharge";
+
 function UnitCard() {
+	const Container = styled("div")({
+		padding: "",
+		backgroundColor: "silver",
+		border: "double indigo 5px",
+	});
+
 	const Green = styled("button")({
-		padding: "15px",
+		padding: "",
 		margin: "40px",
 		backgroundColor: "green",
 		fontWeight: 900,
-		textDecoration: "underline",
-		borderRadius: "15px",
+		textDecoration: "",
+		borderRadius: "",
 	});
 
 	const Red = styled("button")({
@@ -26,25 +37,43 @@ function UnitCard() {
 		color: "black",
 		borderRadius: "20px",
 		textDecoration: "underline",
+		textTransform: "uppercase",
 	});
 
 	return (
 		<>
-			<Red>HHHHHHHHH</Red>
-			<Green>fffffffffff</Green>
+			<Container>
+				<Red>D0ubleD0m1nation</Red>
+				<Green>D0ubleD0m1nation</Green>
+				<p>
+					aapd recus'andae' fugit ducim'us pariatur voluptas suscipit.
+					(Nisi ex reppppppiciendis 'officia' officiis. Debitis
+					deserunt') dolorem. hh( Dolo )res sit quos sint nesciunt
+					natus fugiat aut 'laudantiu'm nisi.
+				</p>
+				<Red>
+					Perferendis quam exercitationem repellendus eum. Quis
+					conseuaur incidunt ysipqui et earum aut ea. Occaecati
+					voluptatum" nam explicabo amet et.
+				</Red>
+			</Container>
 		</>
 	);
 }
 
-/*Page 22*/
-/*Page 41*/
-/*Type Selectors*/
-/*Type 67 PseudoSelectors*/
-/*85 style inheritance*/
-/*91 - 115 style inheritance*/
-/*98 - 122 Inheritrd Styles Accamulation*/
-/*111-135 Cascade in ACTION*/
-/*116-140 Cascade in ACTION*/
-/*116-140 Fonts Applied Google Fonts*/
+/* TODO: Qouc'kkka' dosent work
 
+/*
+
+""Ty"pe S"
+cage -41-
+s85 style/ i/nherinheritances
+Type 67 PseudoSelectors
+98 - 122 Inheritrd Styles Accamulation
+111-135 Casca in ACTION
+116-140 Cascade in ACTN
+116-140 Fonts Applied oogle Fonts
+123
+Plaing with multicursor$s
+*/
 export default UnitCard;
