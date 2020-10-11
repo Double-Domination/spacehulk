2e1d2a7008bbb0f8a5d2cb0e9aa569f7131ed010
D0m1nator <squremeta@gmail.com>
2020-10-11 16:07:21 +0700

    Playing with jss 3

--- src/components/UnitCard/UnitCard.js
+++ src/components/UnitCard/UnitCard.js
@@ -6,106 +6,108 @@
  *
  * Unit Card
  */
-import React from "react";
-import { render } from "react-dom";
-import { createUseStyles, ThemeProvider, useTheme } from "react-jss";
-import styled from "styled-jss";
-/* import fontGlobal from "../AssetsGlobal/vt323.woff"; */
-document.title = "CSS Manual TurboCharge";
+import React from 'react';
+import { render } from 'react-dom';
+import { createUseStyles, ThemeProvider, useTheme } from 'react-jss';
+import styled from 'styled-jss';
+/* import fontGlobal from '../AssetsGlobal/vt323.woff'; */
+document.title = 'CSS Manual TurboCharge';
 
 const stlDefinition = createUseStyles({
+	typographyMixin: {
+		fontSize: '.75rem',
+		fontStyle: 'bold',
+		fontWeight: '200',
+		textTransform: 'capitalize',
+		fontVariant: 'small-caps',
+		textDecoration: 'overline underline',
+		letterSpacing: '-1px',
+		wordSpacing: '2px',
+		textIndent: '4em',
+		color: 'white',
+		textAllign: 'center',
+
+	},
+	
 	blackrage: {
-		backgroundColor: "silver",
-		padding: "15px",
-		color: "black",
-		border: "double 8px #FFA",
-		textShadow: "-5px 5px 0px red, 5px -5px 0px teal",
-		textAllign: "",
-		/*
-			"@font-face": {
-			fontFamily: " vt323regular ",
-			src: "url(../AssetsGlobal/vt323.woff)",
-		},
-		*/
+		backgroundColor: 'silver',
+		padding: '15px',
+		color: 'black',
+		border: 'double 8px #FFA',
+		textShadow: '-5px 5px 0px red, 5px -5px 0px teal',
+		textAllign: 'center',
 	},
 	triBorder: {
-		lineHeight: "75%",
-		width: "300px",
-		margin: "0 auto",
-		dislay: "block",
-		boxSizing: "border-box",
+		lineHeight: '75%',
+		width: '90%',
+		margin: '0 auto',
+		dislay: 'block',
+		boxSizing: 'border-box',
 		boxShadow:
-			"-5px -5px 0px pink, 5px 5px 0px tomato, -5px 5px 0px red, 5px -5px 0px orange",
-		textDecoration: "none",
-		backgroundColor: "white",
-		border: "solid black 9px",
-		"& p::first-letter": {
-			fontSize: "40px",
-			color: "gold",
+			'-5px -5px 0px pink, 5px 5px 0px tomato, -5px 5px 0px red, 5px -5px 0px orange',
+		textDecoration: 'none',
+		backgroundColor: 'white',
+		border: 'solid black 9px',
+		'& p::first-letter': {
+			fontSize: '40px',
+			color: 'gold',
 		},
 	},
+
 });
 
 function UnitCard() {
 	const stlUnitCard = stlDefinition();
-	const Container = styled("div")({
-		backgroundColor: "silver",
-		fontSize: ".75rem",
-		fontStyle: "italic",
-		fontWeight: "200",
-		textTransform: "capitalize",
-		fontVariant: "small-caps",
-		textDecoration: "overline underline",
-		letterSpacing: "-1px",
-		wordSpacing: "2px",
-		textIndent: "4em",
+	
+	const Container = styled('div')({
+		backgroundColor: 'silver',
+		
+		
 	});
 
-	const Green = styled("button")({
-		padding: "5px",
-		margin: "40px",
-		backgroundColor: "green",
+	const Green = styled('button')({
+		padding: '5px',
+		margin: '40px',
+		backgroundColor: 'green',
 		fontWeight: 900,
-		textDecoration: "underline",
-		borderRadius: "61px",
+		textDecoration: 'underline',
+		borderRadius: '61px',
+	});
+	const TemlateLiteralsOl = styled('ol')({
+		border: 'solid black 5px',
+		borderradius:' 5px',
+		padding: '12px',
+		backgroundColor:'gold',
+		color:'violet',
+		
 	});
 
-	const Red = styled("button")({
-		backgroundColor: "red",
-		color: "black",
-		borderRadius: "20px",
-		textDecoration: "underline",
-		textTransform: "uppercase",
+
+	const Red = styled(Green)({
+		backgroundColor: 'white',
+		color:'violet'
+	/* 	backgroundColor: 'red',
+		color: 'black',
+		borderRadius: '20px',
+		textDecoration: 'underline',
+		textTransform: 'uppercase', */
 	});
 
-	const ViText = styled("div")({
-		fontSize: "1em",
+	const ViText = styled('div')({
+		fontSize: '1em',
 	});
 
 	return (
-		<Container className={stlUnitCard.triBorder}>
+		<Container >
 			<Red>D0ubleD0m1nation</Red>
 			<Green>D0ubleD0m1nation</Green>
-			<p className={stlUnitCard.blackrage}>
-				aapd recus'andae' fugit ducim'us pariatur voluptas suscipit.
-				(Nisi ex reppppppiciendis 'officia' officiis. Debitis deserunt')
-				dolorem. hh( Dolo )res sit quos sint nesciunt natus fugiat aut
-				'laudantiu'm nisi.
-			</p>
-
-			<p className={stlUnitCard.triBorder}>
-				Hic consequatur rerum laboriosam ratione nobis earum.
-			</p>
-			<p>gdfgdfg</p>
-			<p>gsgdfgsd</p>
-			<p>gsdgsdgsd gsdfgsdg dsgsdg dgsdgsd gdsfg</p>
-			<p>gsdgsdgsd gsdfgsdg dsgsdg dgsdgsd gdsfg</p>
-			<p>gsdgsdgsd gsdfgsdg dsgsdg dgsdgsd gdsfg</p>
-			<p>gsdgsdgsd gsdfgsdg dsgsdg dgsdgsd gdsfg</p>
-			<p>
-				Adipisci autem magnam perspiciatis nesciunt veniam cum sint.
-				Provident officia ut. Voluptas ab praesentium ut est.
-			</p>
+			
+			<TemlateLiteralsOl>
+				<li>dubas</li>
+				<li>bombud</li>
+				<li>uas</li>
+				<li>METILXXX</li>
+			</TemlateLiteralsOl>
 		</Container>
 	);
 }
@@ -114,7 +116,7 @@ function UnitCard() {
 
 /*
 
-""Ty"pe S"
+''Ty'pe S'
 cage -41-
 s85 style/ i/nherinheritances
 Type 67 PseudoSelectors
