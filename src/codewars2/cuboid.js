5fcaa9349eccb22e26e24fb1bb2d98ab912a66dd
Double-Domination <squremeta@gmail.com>
2021-06-30 18:53:26 +0700

    feat: play with object methods

--- src/codewars2/cuboid.js
+++ src/codewars2/cuboid.js
@@ -1,3 +1,12 @@
+// class Cube extends Cuboid {
+//   constructor(length) {
+//     super(length, length, length);
+//   }
+// }
+
+
+
+
 class Cuboid {
   constructor(length, width, height) {
     this.length = length;
@@ -22,10 +31,97 @@ class Cuboid {
   get volume() {
     return this.length * this.height * this.width;
   }
+
+
+  showEntries() {
+//     let result = [];
+//     for (const key in this) {
+//         const element = this[key];
+//         result.push(`${key} >>> ${element}`);
+//     }
+
+//     result = result.join(
+//       `
+// `);
+
+//     return console.log(result);
+
+    
+    
+    const result = Object.entries(this);
+    return console.log(result.join('\n'));;
+  }
+
+
+  showMethods=()=> {
+    const result = Object.getOwnPropertyDescriptor(this);
+    return console.log(result);
+  }
+
+
+  showConstructor=()=> {
+    const result = this.constructor;
+    return console.log(result);
+  }
+
+  showDescriptors=()=> {
+    const result = Object.getOwnPropertyDescriptors(this);
+    return console.log(result);;
+  }
+
+  showPrototype=()=> {
+    const result = Object.getPrototypeOf(this);
+    return result;
+  }
+
 }
 
-class Cube extends Cuboid {
-  constructor(length) {
-    super(length, length, length);
+class Cubinator extends Cuboid{
+  constructor(lendth, width, height) {
+    super(lendth, width, height);
+    
+    this.cubinatorProp = ' this is cubinator';
   }
+  
 }
+
+
+
+
+
+let metil = new Cuboid(2, 3, 4);
+let extTest = new Cubinator(4, 3, 2);
+
+//console.log(extTest.
+//console.log(metil.constructor)
+
+function dubas() {
+  return "dolbas";
+}
+
+const stringifed = dubas.prototype;
+console.log(stringifed);
+
+//console.log(metil.showPrototype());
+// metil.showinfo()
+
+// metil.showM();
+
+// metil.showEntries();
+
+// metil.showConstructor();
+
+// metil.showDescriptors();
+// try to view prototype from different object
+
+// let stealPrototype = metil.prototype;
+
+// const toShowPrototype = Object.getOwnPropertyDescriptors(stealPrototype.prototype);
+// console.log(toShowPrototype);
+
+
+
+
+
+
+
